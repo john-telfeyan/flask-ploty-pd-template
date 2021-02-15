6ae2f65811a6cc4f296f451d9ddefe9f8d04d756
John Telfeyan <john.telfeyan@gmail.com>
2021-02-14 22:50:45 -0500

    bug fix

--- flask_app.py
+++ flask_app.py
@@ -16,12 +16,12 @@ Distribution: MIT Opens Source Copyright; Full permisions here:
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello Azure!</h1>"
-
-"""
-if __name__=="__main__":
-"""
+def home():
+    return "Hello, World!"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
